5d46f7747d9c1fbe295020f8ccb47dfe22c55f04
BayMx19 <isaimanmuhammad1@gmail.com>
2025-01-04 13:15:16 +0700

    Products Described, -photos

--- src/views/FemileView.vue
+++ src/views/FemileView.vue
@@ -2,7 +2,272 @@
 import NavBarView from "@/components/layouts/navbar.vue";
 </script>
 <template>
-  <main data-spy="scroll" data-target="#navbar" data-offset="30">
+  <main>
     <NavBarView />
+    <header class="bg-gradient-femile" id="home">
+      <div class="container mt-5">
+        <h1 class="font-weight-bold">FEMILE</h1>
+        <p class="tagline">
+          Rasakan Keajaiban Kesehatan dengan GOMILK: Susu Kambing Etawa Plus
+          Herbal Pilihan Terbaik! 🌿✨.
+        </p>
+      </div>
+      <div class="img-holder mt-3">
+        <img
+          src="\public\assets\img\products\gomilk\gomilk-header.png"
+          alt="gomilk"
+          class="img-fluid"
+        />
+      </div>
+    </header>
+
+    <div class="section light-bg" id="tentang">
+      <div class="container px-5">
+        <div class="row gx-5 justify-content-center">
+          <div class="col-xxl-8">
+            <div class="text-center my-5">
+              <h2 class="font-weight-bold femile">Tentang FEMILE</h2>
+              <p class="text-dark mt-5">
+                <strong>FEMILE</strong> adalah satu formulasi khusus berbahan
+                dasar perahan susu kambing etawa yang di kombinasikan dengan
+                ekstrak herbal pilihan, yaitu daun kelor, daun salam, dan herbal
+                lainnya yang sudah lama dikenal khasiatnya oleh masyarakat untuk
+                terapi berbagai macam penyakit degeneratif. Susu kambing etawa
+                adalah minuman terbaik di dunia setelah ASI dan banyak sekali
+                manfaat bagi kesehatan. Salah satu susu hewani yang sangat cocok
+                bagi sistem pencernaan manusia, bahkan untuk balita ataupun
+                lansia sekalipun. Dari hasil penelitian para ahli dan bukti
+                sejarah membuktikan bahwa susu kambing mempunyai kelebihan
+                dibanding dengan susu hewan lainnya.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="img-gallery owl-carousel owl-theme">
+          <img
+            src="\public\assets\img\products\gomilk\bacaan/1.jpg"
+            alt="image"
+          />
+          <img
+            src="\public\assets\img\products\gomilk\bacaan/2.jpg"
+            alt="image"
+          />
+          <img
+            src="\public\assets\img\products\gomilk\bacaan/3.jpg"
+            alt="image"
+          />
+          <img
+            src="\public\assets\img\products\gomilk\bacaan/4.jpg"
+            alt="image"
+          />
+          <img
+            src="\public\assets\img\products\gomilk\bacaan/5.jpg"
+            alt="image"
+          />
+        </div>
+      </div>
+    </div>
+    <!-- // end .section -->
+    <div class="section" id="kelebihan">
+      <div class="container text-align-center">
+        <h2 class="font-weight-bold femile">Kelebihan FEMILE</h2>
+        <div class="row">
+          <div class="col-sm-4 col-lg-4">
+            <img
+              src="\public\assets\img\products\gomilk\gomilk-1.png"
+              alt="perspective phone"
+              class="img-fluid"
+            />
+          </div>
+          <div class="col-sm-8 col-lg-8 py-5">
+            <p class="mb-4 text-dark">
+              Berikut ini adalah beberapa keunggulan yang dimiliki oleh
+              <strong>FEMILE</strong>, antara lain :<br />
+              ✔️ Halal<br />
+              ✔️ Sudah Berizin BPOM<br />
+              ✔️ Tidak bau amis kambing <br />
+              ✔️ Susu Kambing Etawa Plus Herbal pertama di Indonesia<br />
+              ✔️ Tinggi Vitamin C dan Kalsium<br />
+              ✔️ Rasanya lebih lezat<br />
+              ✔️ Rasanya enak dan gurih<br />
+              ✔️ Cocok untuk semua usia<br />
+              ✔️ Susu terbaik setelah ASI<br />
+              ✔️ Memiliki banyak khasiat untuk tubuh<br />
+              ✔️ Dari bahan berkualitas<br />
+              ✔️ Menurunkan Tekanan Darah Tinggi, Kolesterol, Asam Urat<br />
+              ✔️ Baik untuk penderita Diabetes, Maag, dan Stroke<br />
+            </p>
+            <a
+              href="https://wa.me/+6282191927762?text=Halo%20saya%20tertarik%20dengan%20GOMILK..."
+              class="btn btn-primary femile"
+              ><svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-whatsapp"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  d="M13.601 2.326A7.85 7.85 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.9 7.9 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.9 7.9 0 0 0 13.6 2.326zM7.994 14.521a6.6 6.6 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.56 6.56 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592m3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.73.73 0 0 0-.529.247c-.182.198-.691.677-.691 1.654s.71 1.916.81 2.049c.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232"
+                />
+              </svg>
+              Pesan Sekarang</a
+            >
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- // end .section -->
+
+    <div class="section light-bg" id="harga">
+      <div class="container">
+        <div class="section-title">
+          <h2 class="font-weight-bold">Harga Terjangkau!</h2>
+        </div>
+
+        <div class="card-deck">
+          <div class="card pricing popular">
+            <div class="card-head">
+              <h4 class="font-weight-bold femile">Original FEMILE</h4>
+              <span class="price femile">Rp. 35.000,-<sub>/pcs</sub></span>
+            </div>
+
+            <div class="card-body">
+              <a
+                href="https://wa.me/+6282191927762?text=Halo%20saya%20tertarik%20dengan%20GOMILK%20Rasa%20Strawberry..."
+                class="btn btn-primary femile btn-lg btn-block"
+                >Pesan</a
+              >
+            </div>
+          </div>
+        </div>
+
+        <div class="keunggulan-berbelanja">
+          <h4 class="text-dark text-align-center mt-5">
+            Keunggulan Berbelanja <strong>FEMILE</strong> pada Kami :
+          </h4>
+          <p class="mb-4 text-dark">
+            ✔️ Gratis Konsultasi 24 Jam<br />
+            ✔️ Gratis Ongkir Wilayah Surabaya dan Sekitarnya<br />
+            ✔️ Bisa COD (Cash On Delivery)atau Bayar di Rumah<br />
+            ✔️ Pembayaran Bisa via Transfer<br />
+          </p>
+        </div>
+        <!-- // end .pricing -->
+      </div>
+    </div>
+    <!-- // end .section -->
+    <div class="section" id="testi">
+      <div class="container">
+        <div class="section-title">
+          <h2 class="font-weight-bold femile">Testimoni Pelanggan FEMILE</h2>
+        </div>
+
+        <div class="img-gallery owl-carousel owl-theme">
+          <img
+            src="\public\assets\img\products\gomilk\testi/1.jpg"
+            alt="image"
+          />
+          <img
+            src="\public\assets\img\products\gomilk\testi/2.jpg"
+            alt="image"
+          />
+          <img
+            src="\public\assets\img\products\gomilk\testi/3.jpg"
+            alt="image"
+          />
+          <img
+            src="\public\assets\img\products\gomilk\testi/4.jpg"
+            alt="image"
+          />
+          <img
+            src="\public\assets\img\products\gomilk\testi/5.jpg"
+            alt="image"
+          />
+          <img
+            src="\public\assets\img\products\gomilk\testi/6.jpg"
+            alt="image"
+          />
+          <img
+            src="\public\assets\img\products\gomilk\testi/7.jpg"
+            alt="image"
+          />
+          <img
+            src="\public\assets\img\products\gomilk\testi/8.jpg"
+            alt="image"
+          />
+          <img
+            src="\public\assets\img\products\gomilk\testi/9.jpg"
+            alt="image"
+          />
+          <img
+            src="\public\assets\img\products\gomilk\testi/10.jpg"
+            alt="image"
+          />
+          <img
+            src="\public\assets\img\products\gomilk\testi/11.jpg"
+            alt="image"
+          />
+          <img
+            src="\public\assets\img\products\gomilk\testi/12.jpg"
+            alt="image"
+          />
+          <img
+            src="\public\assets\img\products\gomilk\testi/13.jpg"
+            alt="image"
+          />
+          <img
+            src="\public\assets\img\products\gomilk\testi/14.jpg"
+            alt="image"
+          />
+          <img
+            src="\public\assets\img\products\gomilk\testi/15.jpg"
+            alt="image"
+          />
+        </div>
+      </div>
+    </div>
+    <!-- // end .section -->
+
+    <!-- // end .section -->
+
+    <div class="section bg-gradient-femile p-4">
+      <div class="container">
+        <div class="call-to-action">
+          <h2 class="font-weight-bold femile mb-3">
+            Tertarik untuk mencoba <strong>FEMILE</strong>?
+          </h2>
+          <br />
+
+          <a
+            href="https://wa.me/+6282191927762?text=Halo%20saya%20tertarik%20dengan%20GOMILK..."
+            class="btn btn-hubungi femile mt-1 mb-3"
+            ><svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-whatsapp"
+              viewBox="0 0 16 16"
+            >
+              <path
+                d="M13.601 2.326A7.85 7.85 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.9 7.9 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.9 7.9 0 0 0 13.6 2.326zM7.994 14.521a6.6 6.6 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.56 6.56 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592m3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.73.73 0 0 0-.529.247c-.182.198-.691.677-.691 1.654s.71 1.916.81 2.049c.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232"
+              />
+            </svg>
+            Pesan Sekarang</a
+          ><br />
+
+          <small class="text-white">atau</small><br />
+          <a
+            href="https://www.tokopedia.com/imnherbalstore"
+            class="btn btn-cart femile mt-3 mb-3"
+          >
+            Tokopedia</a
+          >
+        </div>
+      </div>
+    </div>
+    <!-- // end .section -->
   </main>
 </template>
